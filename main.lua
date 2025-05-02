b12667ce11161bd546b83ec601b4d00c6ee1e6e9
2tuffsites <thyroparathy11@gmail.com>
2025-05-02 14:19:44 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,47 +1,94 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
 local Window = Rayfield:CreateWindow({
    Name = "2tuff cheats",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   Icon = 0,
    LoadingTitle = "cheats brah",
    LoadingSubtitle = "by luc",
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+   Theme = "Dark Blue",
 
    DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+   DisableBuildWarnings = false,
 
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
+      FolderName = nil,
       FileName = "2tuff cheats"
    },
 
    Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
    },
 
-   KeySystem = true, -- Set this to true to use our key system
+   KeySystem = true,
    KeySettings = {
       Title = "Untitled",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "KeySystem", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Rahh"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Note = "No method of obtaining the key is provided",
+      FileName = "KeySystem",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"Rahh"}
    }
 })
-local PlayerTab = Window:CreateTab("Player", 4483362458) -- Title, Image
-local Slider = Tab:CreateSlider({
+
+-- PLAYER TAB
+local PlayerTab = Window:CreateTab("Player", 4483362458)
+
+-- SPEED SLIDER
+local Slider = PlayerTab:CreateSlider({
    Name = "Speed",
-   Range = {26, 500},
+   Range = {16, 500},
    Increment = 1,
    Suffix = "Speed",
-   CurrentValue = 10,
-   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   CurrentValue = 16,
+   Flag = "Slider1",
    Callback = function(Value)
-   -- The function that takes place when the slider changes
-   -- The variable (Value) is a number which correlates to the value the slider is currently at
+      local char = game.Players.LocalPlayer.Character
+      if char and char:FindFirstChild("Humanoid") then
+         char.Humanoid.WalkSpeed = Value
+      end
    end,
 })
+
+-- REJOIN VOICE CHAT BUTTON
+PlayerTab:CreateButton({
+   Name = "Rejoin Voice Chat",
+   Callback = function()
+      game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+   end,
+})
+
+-- TELEPORT TO PLAYER DROPDOWN
+local TeleportDropdown = PlayerTab:CreateDropdown({
+   Name = "Teleport To Player",
+   Options = {},
+   CurrentOption = nil,
+   Flag = "TPDropdown",
+   Callback = function(playerName)
+      local target = game.Players:FindFirstChild(playerName)
+      if target and target.Character and target.Character:FindFirstChild("HumanoidRootPart") then
+         local hrp = game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+         if hrp then
+            hrp.CFrame = target.Character.HumanoidRootPart.CFrame + Vector3.new(2, 0, 2)
+         end
+      end
+   end,
+})
+
+-- REFRESH PLAYER LIST FUNCTION
+local function updatePlayers()
+   local names = {}
+   for _, player in pairs(game.Players:GetPlayers()) do
+      if player ~= game.Players.LocalPlayer then
+         table.insert(names, player.Name)
+      end
+   end
+   TeleportDropdown:Refresh(names, true)
+end
+
+updatePlayers()
+game.Players.PlayerAdded:Connect(updatePlayers)
+game.Players.PlayerRemoving:Connect(updatePlayers)
