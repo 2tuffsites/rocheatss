036ac8326cec4cba4bcf85f18796158a4fb91222
2tuffsites <thyroparathy11@gmail.com>
2025-05-06 18:07:32 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -155,7 +155,7 @@ Callback = function(Value)
 if Value then
 if not baseplate then
 baseplate = Instance.new("Part")
-baseplate.Size = Vector3.new(150000, 0, 150000)
+baseplate.Size = Vector3.new(1000000, 0, 1000000)
 baseplate.Anchored = true
 baseplate.Position = Vector3.new(0, 0, 0)
 baseplate.Color = selectedColor
